87b5ab784f4e534e77bc504b64aa84f8fc32d6b2
goodeeit <gt_min@naver.com>
2023-09-18 11:55:23 +0900

    20230918

--- 03_jsp/src/main/java/ex06_session/ClearCart.java
+++ 03_jsp/src/main/java/ex06_session/ClearCart.java
@@ -9,16 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class Login
+ * Servlet implementation class ClearCart
  */
-@WebServlet("/login")
-public class Login extends HttpServlet {
+@WebServlet("/clearCart")
+public class ClearCart extends HttpServlet {
   private static final long serialVersionUID = 1L;
        
   /**
    * @see HttpServlet#HttpServlet()
    */
-  public Login() {
+  public ClearCart() {
     super();
     // TODO Auto-generated constructor stub
   }
@@ -27,21 +27,10 @@ public class Login extends HttpServlet {
    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
-    // 요청 인코딩
-    request.setCharacterEncoding("UTF-8");
-    
-    // 요청 파라미터
-    String id = request.getParameter("id");
-    String pw = request.getParameter("pw");
-    
-    // 로그인 성공 규칙 : id와 pw가 동일하면 로그인 성공으로 가정하고 풀이
-    if(id.equals(pw)) {
-      // 로그인 처리 : session에 id를 저장해 두기
-      HttpSession session = request.getSession();
-      session.setAttribute("id", id);
-      session.setMaxInactiveInterval(60 * 10);  // 10분간 세션 유지
-    }
+
+    // 장바구니 비우기 : session에 저장된 cart 지우기
+    HttpSession session = request.getSession();
+    session.removeAttribute("cart");
     
     // main 화면으로 되돌아가기
     response.sendRedirect(request.getContextPath() + "/ex06_session/main.jsp");
