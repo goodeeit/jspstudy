e17afb799896ceae77d3bf034d571541f630d341
goodeeit <gt_min@naver.com>
2023-09-19 16:29:40 +0900

    20230919

--- 07_dbcp/src/main/java/repository/BoardDao.java
+++ 07_dbcp/src/main/java/repository/BoardDao.java
@@ -8,6 +8,8 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import domain.BoardDto;
+
 public class BoardDao {
 
   // 모든 메소드가 공동으로 사용할 객체 선언
@@ -34,9 +36,53 @@ public class BoardDao {
     return dao;
   }
   
+  // 자원 반납 메소드
+  public void close() {
+    try {
+      if(rs != null) rs.close();
+      if(ps != null) ps.close();
+      if(con != null) con.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
   
-  
-  
+  // 게시글 등록 메소드
+  public int register(BoardDto dto) {
+    
+    // 등록 결과 선언 (insert 실행 결과는 삽입된 행의 개수이다.)
+    int insertResult = 0;
+    
+    try {
+      
+      // Connection Pool에서 Connection을 하나 받아온다.
+      // Connection Pool 관리는 DataSource 객체가 수행한다.
+      
+      con = dataSource.getConnection();
+      
+      // 쿼리문 작성 
+      String sql = "INSERT INTO BOARD_T(BOARD_NO, TITLE, CONTENT, MODIFIED_AT, CREATED_AT) VALUES (BOARD_SEQ.NEXTVAL, ?, ?, SYSDATE, SYSDATE)";
+      
+      // ps 객체 생성 (쿼리문 실행을 담당하는 객체)
+      ps = con.prepareStatement(sql);
+      
+      // 쿼리문의 변수(?로 처리된 부분)에 값을 전달
+      ps.setString(1, dto.getTitle());   // 1번째 물음표(?)에 dto.getTitle() 전달하기
+      ps.setString(2, dto.getContent()); // 2번째 물음표(?)에 dto.getContent() 전달하기
+      
+      // 쿼리문의 실행
+      insertResult = ps.executeUpdate();
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      close();
+    }
+    
+    // 등록 결과 반환
+    return insertResult;
+    
+  }
   
   
   
